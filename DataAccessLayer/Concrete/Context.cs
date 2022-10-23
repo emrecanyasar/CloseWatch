66fc2f889513e37c2803aa9dc32298bbf3bf860e
Emrecan <emrecanyasar8@outlook.com>
2022-10-23 13:00:35 +0300

    connection string tanımlandı

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        //connection string tanımlandı
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-7JITH0C;database=CoreBlogDb; integrated security=true;");
+        }
+    }
+}

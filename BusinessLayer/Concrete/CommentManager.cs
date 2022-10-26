40694ba5932956606be3e1279bb47e956285e771
Emrecan <emrecanyasar8@outlook.com>
2022-10-27 00:17:55 +0300

    CommentList View Component created

---
+++ BusinessLayer/Concrete/CommentManager.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CommentManager : ICommentServices
+    {
+        ICommentDal _commentDal;
+
+        public CommentManager(ICommentDal commentDal)
+        {
+            _commentDal = commentDal;
+        }
+
+        public void CommentAdd(Comment comment)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Comment> GetList(int id)
+        {
+           return  _commentDal.GetListAll(x=>x.BlogID==id);
+        }
+    }
+}
---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,32 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    Id = 1,
+                    UserName ="Emre"
+                },
+                new UserComment
+                {
+                    Id = 2,
+                    UserName ="Can"
+                },
+                new UserComment
+                {
+                    Id=3,
+                    UserName ="Yasar"
+                }
+            };
+            return View(commentvalues);
+        }
+    }
+}

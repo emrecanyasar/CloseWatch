1966f24127b282a307d824d4ef5b9c5e92b4804b
Emrecan <emrecanyasar8@outlook.com>
2022-10-27 00:52:09 +0300

    Blog Comments Listed

---
+++ CoreDemo/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Comment
+{
+    public class CommentListByBlog : ViewComponent
+    {
+        CommentManager cm = new CommentManager(new EfCommentRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values=cm.GetList(7);
+            return View(values);
+        }
+
+    }
+}

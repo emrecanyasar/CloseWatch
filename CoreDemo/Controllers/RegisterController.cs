f569b8d33efa0aa9685444529ea5cf15a1ac3f8e
Emrecan <emrecanyasar8@outlook.com>
2022-10-29 23:44:25 +0300

    Fluent Validation Core

--- CoreDemo/Controllers/RegisterController.cs
+++ CoreDemo/Controllers/RegisterController.cs
@@ -3,6 +3,8 @@ using BusinessLayer.Abstract;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using BusinessLayer.ValidationRules;
+using FluentValidation.Results;
 
 namespace CoreDemo.Controllers
 {
@@ -19,10 +21,24 @@ namespace CoreDemo.Controllers
         [HttpPost]
         public IActionResult Index(Writer p)
         {
-            p.WriterStatus = true;
-            p.WriterAbout = "Denem Test";
-            wm.WriterAdd(p);
-            return RedirectToAction("Index","Blog");
+            WriterValidator vw =new WriterValidator();
+            ValidationResult results=vw.Validate(p);
+            if (results.IsValid)
+            {
+                p.WriterStatus = true;
+                p.WriterAbout = "Denem Test";
+                wm.WriterAdd(p);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach(var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+            }
+            return View();
+           
         }
     }
 }

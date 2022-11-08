9dc264a490372fa4e84a0739527358e487626535
Emrecan <emrecanyasar8@outlook.com>
2022-11-08 16:15:41 +0300

    Writer page header view components created

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -22,5 +22,22 @@ namespace CoreDemo.Controllers
         {
             return View();
         }
+
+        [AllowAnonymous]
+        public PartialViewResult WriterNavbarPartial()
+        {
+            return PartialView();
+        }
+
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+        {
+            return PartialView();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterHeaderPartial()
+        {
+            return PartialView();
+        }
     }
 }

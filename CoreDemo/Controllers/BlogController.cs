ebd7fef092f6ac66e0a410ea3b8d0ff281244465
Emrecan <emrecanyasar8@outlook.com>
2022-11-08 17:31:17 +0300

    Some fixes

--- CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/Controllers/BlogController.cs
@@ -22,6 +22,10 @@ namespace CoreDemo.Controllers
             var values =bm.GetBlogByID(id);
             return View(values);
         }
-        
+        public IActionResult BlogListByWriter() 
+        {
+            var values= bm.GetBlogListBytWriter(1);
+            return View(values);
+        }
     }
 }
